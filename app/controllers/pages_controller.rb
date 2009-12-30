431976e42746b26bd90c96a10f20fdbee1b4f685
Mathias <mathiasmex@gmail.com>
2009-12-31 00:51:43 +0100

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+	@title = "Start"
   end
 
   def contact
+	@title = "Kontakt"
+  end
+
+  def about
+	@title = "Über mich"
   end
 
 end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe PagesController do
+  integrate_views
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_tag("title",
+                           "matze.msh::WebDesign | Start")
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_tag("title",
+                           "matze.msh::WebDesign | Kontakt")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_tag("title",
+                           "matze.msh::WebDesign | Über mich")
+    end
+  end
+
+end
