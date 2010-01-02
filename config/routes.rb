d45a1842e997f35b9893b7029f74427e77b7653b
Mathias <mathiasmex@gmail.com>
2010-01-02 01:08:55 +0100

    vendors missing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+
+  map.resource :session
+
+  map.activate '/activate/:activation_code', 
+    :controller => 'users', 
+    :action => 'activate', 
+    :activation_code => nil
+
+  map.signup '/signup', 
+    :controller => 'users', 
+    :action => 'new'
+
+  map.login '/login', 
+    :controller => 'sessions', 
+    :action => 'new'
+
+  map.logout '/logout', 
+    :controller => 'sessions', 
+    :action => 'destroy'
+
   map.contact '/contact',   :controller => 'pages', :action => 'contact'
   map.about   '/about',     :controller => 'pages', :action => 'about'
   map.service    '/service',   :controller => 'pages', :action => 'service'
--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20100101141718) do
+
+  create_table "users", :force => true do |t|
+    t.string   "login"
+    t.string   "email"
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token"
+    t.datetime "remember_token_expires_at"
+    t.string   "activation_code",           :limit => 40
+    t.datetime "activated_at"
+  end
 
 end
